84193b0f612465fe1039697842002d3730fc8fef
Nidhi <nid200128@gmail.com>
2022-06-29 12:11:53 +0530

    Keyword_Argument

---
+++ 21_Keword_Arguments.py
@@ -0,0 +1,16 @@
+# with keyword argument, order of prameters doesn't matter. It matters in positional argument, in which we simply pass the value for parameters.
+#   keyword argument shouls always come after positional argument.
+
+
+def greet_user(first_name, last_name):
+
+    print(f"Hi {first_name}{last_name}!")
+    print('Welcome Aboard')
+
+
+print("Start")
+greet_user(first_name="Nidhi", last_name="Kumari")  # Keyword argument
+greet_user("Nidhi", last_name="Kumari")
+#   keyword argument shouls always come after positional argument.
+
+print("Finish")
